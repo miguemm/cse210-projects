f85112b63343607febadbb8bbf362a5db32c29c1
Miguel Andres Mosquera <mandresmosquera@gmail.com>
2024-01-06 11:23:49 -0700

    C# Prep 3 - Solved

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        Random random = new Random();
+        int magicNumber = random.Next(1,100);
+        int guess;
+        int count = 0;
+        bool play = true;
+
+        while (play)
+        {
+            Console.WriteLine("Welcome to Try to guess");
+            
+            do
+            {
+                count++;
+                Console.WriteLine("What is your guess?");
+                guess = int.Parse(Console.ReadLine());
+
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }else{
+                    Console.WriteLine("Higher");
+                }
+
+            } while (guess != magicNumber);
+            Console.WriteLine($"You guessed it!. # attempts: {count}");
+
+            Console.WriteLine("Keep playing? (yes/no)");
+            if (Console.ReadLine() != "yes")
+            {
+                play = false;
+            }
+
+        }
+
+
     }
 }
