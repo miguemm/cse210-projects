74b31b1e5b0abaecd057eef9b1e747cee87dca34
Miguel Andres Mosquera <mandresmosquera@gmail.com>
2024-01-06 11:00:29 -0700

    C# Prep 2 - solved

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Type your grade percentage");
+        int gradePercentage = int.Parse(Console.ReadLine());
+        string grade; 
+
+        if (gradePercentage >= 90)
+        {
+            grade = "A";
+        }
+        else if (gradePercentage >=80)
+        {
+            grade = "B";
+        }
+        else if (gradePercentage >=70)
+        {
+            grade = "C";
+        }
+        else if (gradePercentage >=60)
+        {
+            grade = "D";
+        }else{
+            grade = "F";
+        }
+
+        int lastDigit = gradePercentage % 10;
+        string sign;
+
+        if (lastDigit >= 7 && grade != "A" && grade != "F")
+            sign = "+";
+        else if (lastDigit < 3 && grade != "F") 
+            sign = "-";
+        else
+            sign = "";
+
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congrats!, you approved the class");
+        }
+
+
+        Console.WriteLine($"Your grade is: {grade}{sign}");
     }
 }
