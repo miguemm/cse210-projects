ac7e3e0d4353167c3048abd1bfba5756006d3124
Miguel Andres Mosquera <mandresmosquera@gmail.com>
2024-01-06 11:47:51 -0700

    C# Prep 4 - solved

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int input;
+        List<int> numbers = new();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do
+        {
+            Console.WriteLine("Enter number:");
+            input  = int.Parse(Console.ReadLine());
+
+            if (input != 0)
+            {
+            numbers.Add(input);
+            }
+
+        } while (input != 0);
+
+        int sum = numbers.Sum();
+        Console.WriteLine($"The sum is: {sum}");
+
+        double average = numbers.Average();
+        Console.WriteLine($"The average is: {average}");
+
+        int maxNumber = numbers.Max();
+        Console.WriteLine($"The largest number is: {maxNumber}");
+
+        numbers.Sort();
+
+        Console.WriteLine("Sorted List:");
+        foreach (var num in numbers)
+        {
+            Console.Write(num + " ");
+        }
     }
 }
